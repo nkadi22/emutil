8e4301ec26e7147dbb5ce88b5f6d9c89aa94478b
nkadi22 <nkadi.lht@gmail.com>
2020-07-28 02:58:35 -0700

    Add include files

---
+++ include/emutil.hpp
@@ -0,0 +1,17 @@
+
+#ifndef __EM_UTIL_H__
+#define __EM_UTIL_H__
+
+#include <cstdio>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <utility>
+#include <getopt.h>
+#include <cstring>
+#include "emprocessor/emprocessor.hpp"
+
+const char * DEFAULT_INPUT_FILE = "em.ds";
+bool _verbose = false;
+
+#endif
